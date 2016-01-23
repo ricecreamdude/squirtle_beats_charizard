5d80aa7ed4900f573778e58ff89a3f93b6c488fc
James Mason <jmason@alumni.reed.edu>
2016-01-23 13:54:53 -0800

    Created a Pokemon API router based on Sabrina's routes.  Has testing.

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-const koa = require('koa');
-const route = require('koa-route');
-const app = koa();
-const render = require(__dirname + '/lib/render');
-var PORT = process.env.PORT || 3000;
-
-app.use(route.get('/get', render.get));
-app.use(route.post('/post', render.post));
-app.use(route.put('/put', render.put));
-app.use(route.delete('/delete', render.delete));
-
-var server = module.exports = exports = app.listen(PORT, () => console.log('Server up on port ' + PORT));
+require('mongoose').connect(process.env.MONGOLAB_URI || 'mongodb://localhost/pokemon');
+module.exports = exports = require('koa')().use(require(__dirname + '/lib/apiRouter').routes()).listen((process.env.PORT || 3000));
