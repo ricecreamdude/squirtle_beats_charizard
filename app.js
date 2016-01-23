7820696adb0be1bfdedd1ae09c71c6a1e7b9eb19
SABRINA TEE <sabrinatee@me.com>
2016-01-23 11:20:38 -0800

    Working POST request - added dependency on co-body module and added gulpfile

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const koa = require('koa');
 const route = require('koa-route');
 const app = koa();
-const render = require(__dirname + '/lib/render')
+const render = require(__dirname + '/lib/render');
 var PORT = process.env.PORT || 3000;
 
-app.use(route.get('/hello', render.hello));
-app.use(route.post('/hello', render.greet));
+app.use(route.get('/get', render.get));
+app.use(route.post('/post', render.post));
 // app.use(route.put('/', ));
 // app.use(route.delete('/', ));
 
